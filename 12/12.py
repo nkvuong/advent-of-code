bda7368a27335ae939e84a8b3335b535835e0b91
Vuong Nguyen <44292934+nkvuong@users.noreply.github.com>
2020-12-13 19:21:47 +0000

    refactor using complex number

--- 12/12.py
+++ 12/12.py
@@ -1,42 +1,11 @@
-from os import error
-
-
-def rotate(point, value):
-    if value % 90 > 0:
-        raise ValueError("rotation must be multiplier of 90")
-
-    multiplier = [[1, 1], [1, -1], [-1, -1], [-1, 1]]
-
-    if point[0] > 0:
-        if point[1] > 0:
-            quadrant = 0
-        else:
-            quadrant = 1
-    else:
-        if point[1] < 0:
-            quadrant = 2
-        else:
-            quadrant = 3
-
-    if value % 180 > 0:
-        temp = point[0]
-        point[0] = point[1]
-        point[1] = temp
-
-    quadrant = (quadrant + int(value/90)) % 4
-
-    point[0] = multiplier[quadrant][0] * abs(point[0])
-    point[1] = multiplier[quadrant][1] * abs(point[1])
-
-
 inputs = ['test1', 'input']
-NORTH, EAST, SOUTH, WEST = 0, 1, 2, 3
-directions = ['N', 'E', 'S', 'W']
 movement = {
-    'N': (1, 1),
-    'E': (0, 1),
-    'S': (1, -1),
-    'W': (0, -1)
+    'N': 1,
+    'E': 1j,
+    'S': -1,
+    'W': -1j,
+    'R': 1j,
+    'L': -1j
 }
 
 for input in inputs:
@@ -45,36 +14,34 @@ for input in inputs:
     with open(f'12/{input}', 'r') as f:
         instructions = [(r[0], int(r[1:])) for r in f.read().split("\n")]
 
-    heading = EAST
-    location = [0, 0]
+    heading = 0 + 1j
+    location = 0 + 0j
 
     for action, value in instructions:
         if action == 'F':
-            action = directions[heading]
+            location += heading * value
         if action in 'NEWS':
-            location[movement[action][0]] += movement[action][1] * value
+            location += movement[action] * value
         elif action in 'LR':
-            heading = (heading + int(value/90) *
-                       (-1 if action == 'L' else 1)) % 4
-        # print(f'Position {location} heading {directions[heading]}')
+            for _ in range((value // 90)):
+                heading *= movement[action]
+        # print(f'Position {location} heading {heading}')
 
-    print(f'Part 1: {sum(map(abs, location))}')
+    print(f'Part 1: {int(abs(location.real) + abs(location.imag))}')
 
     # Part 2
-    location = [0, 0]
-    waypoint = [10, 1]
+    location = 0 + 0j
+    waypoint = 1 + 10j
 
     for action, value in instructions:
-        if action == 'L':
-            action = 'R'
-            value = 360-value
         if action == 'F':
-            location = [x + value * y for x, y in zip(location, waypoint)]
+            location += waypoint * value
         if action in 'NEWS':
-            waypoint[movement[action][0]] += movement[action][1] * value
-        elif action == 'R':
-            rotate(waypoint, value)
+            waypoint += movement[action] * value
+        elif action in 'LR':
+            for _ in range((value // 90)):
+                waypoint *= movement[action]
 
         # print(f'Position {location} waypoint {waypoint}')
 
-    print(f'Part 2: {sum(map(abs, location))}')
+    print(f'Part 2: {int(abs(location.real) + abs(location.imag))}')
