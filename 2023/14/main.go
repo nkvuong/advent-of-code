16cb2917855b776928f4b1b87ffc7d3e00602e36
Vuong <vuong.nguyen@databricks.com>
2023-12-15 23:23:23 +0000

    day 14 part 2

--- 2023/14/main.go
+++ 2023/14/main.go
@@ -16,34 +16,107 @@ func init() {
 	}
 }
 
-func main() {
-
-	load := 0
-	height := strings.Count(input, "\n") + 1
-	roundCount := make(map[int]int)
-	col := 0
-	row := 0
-	for _, rock := range input {
-		switch {
-		case
-			rock == '\n':
-			{
-				col = 0
-				row++
-				continue
-			}
-		case rock == 'O':
-			{
-				roundCount[col]++
-				load += height - roundCount[col] + 1
-			}
-		case rock == '#':
-			{
-				roundCount[col] = row + 1
+func moveNorth(board [][]byte) {
+	for i := 1; i < len(board); i++ {
+		for j := 0; j < len(board[i]); j++ {
+			for k := i - 1; k >= 0; k-- {
+				if board[k][j] == '.' && board[k+1][j] == 'O' {
+					board[k][j] = 'O'
+					board[k+1][j] = '.'
+				}
 			}
 		}
-		col++
 	}
-	fmt.Println("Total load is", load)
+}
 
+func calc(board [][]byte) (sum int) {
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[i]); j++ {
+			if board[i][j] == 'O' {
+				sum += len(board) - i
+			}
+		}
+	}
+	return sum
+}
+
+func rotate(board [][]byte) [][]byte {
+	rows, cols := len(board), len(board[0])
+
+	rotated := make([][]byte, cols)
+	for i := 0; i < cols; i++ {
+		rotated[i] = make([]byte, rows)
+		for j := 0; j < rows; j++ {
+			rotated[i][j] = board[rows-1-j][i]
+		}
+	}
+	return rotated
+}
+
+func key(board [][]byte) (out string) {
+	for _, line := range board {
+		out = out + string(line)
+	}
+	return out
+}
+
+func deepCopy(board [][]byte) (out [][]byte) {
+	out = make([][]byte, len(board))
+	for i, row := range board {
+		out[i] = append([]byte(nil), row...)
+	}
+	return out
+}
+
+func main() {
+
+	lines := strings.Split(input, "\n")
+	board := make([][]byte, 0)
+	for _, line := range lines {
+		board = append(board, []byte(line))
+	}
+
+	// part 1
+	copyBoard := deepCopy(board)
+	moveNorth(copyBoard)
+	fmt.Println("Total load is", calc(copyBoard))
+
+	// part 2
+	const B = 1_000_000_000
+	cache := map[string]int{}
+	result := map[int][][]byte{}
+	current, previous := 0, 0
+	for ; ; current++ {
+		if cache[key(board)] > 0 {
+			if previous == 0 {
+				// first time repeat, reset the cache
+				previous = current
+				cache = map[string]int{}
+			} else {
+				// second time repeat, stop
+				break
+			}
+		} else {
+			cache[key(board)] = current
+			result[current] = deepCopy(board)
+		}
+
+		// North
+		moveNorth(board)
+		//West = rotate + move north
+		board = rotate(board)
+		moveNorth(board)
+		board = rotate(rotate(rotate(board)))
+		//South = rotate x2 + move north
+		board = rotate(rotate(board))
+		moveNorth(board)
+		board = rotate(rotate(board))
+		// East = rotate x3 + move north
+		board = rotate(rotate(rotate(board)))
+		moveNorth(board)
+		board = rotate(board)
+	}
+
+	period := current - previous - 1
+	fmt.Println("Total load after a billion rotations", calc(result[previous+(B-previous)%period]))
 }
